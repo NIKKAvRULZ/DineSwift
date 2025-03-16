b148a988bd3b1ab1f4faba1e571846635be25f0e
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-03-16 21:02:19 +0530

    Implement user registration and login functionality with JWT authentication

--- UserService/index.js
+++ UserService/index.js
@@ -19,3 +19,4 @@ app.get('/', (req, res) => res.send ('User Service Running'));
 
 // Start Server
 const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
