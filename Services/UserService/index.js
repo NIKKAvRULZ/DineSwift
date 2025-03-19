f8cef66a88e789e3100983901681c11e13c7c11f
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-03-19 15:40:05 +0530

    add newline at end of UserService index.js

--- Services/UserService/index.js
+++ Services/UserService/index.js
@@ -20,6 +20,7 @@ mongoose.connect(process.env.MONGO_URI, {
 // Routes
 app.get('/', (req, res) => res.send ('User Service Running'));
 
+
 // Start Server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`User Service running on port ${PORT}`));
