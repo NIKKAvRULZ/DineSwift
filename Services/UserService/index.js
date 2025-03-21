56c4d0511d34b5e6ad286cfaef78beb2a07ef0d0
Nithika <87988890+NIKKAvRULZ@users.noreply.github.com>
2025-03-22 02:10:52 +0530

    Refactor UserService to use a separate database connection module and add CORS support

--- Services/UserService/index.js
+++ Services/UserService/index.js
@@ -1,21 +1,18 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotnev = require('dotenv');
-
+const cors = require('cors');
+const connectDB = require('./config/db');
 const authRoutes = require("./routes/authRoutes");
 
 dotnev.config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-// connect mongodb
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('User Service DB Connected'))
-.catch(err => console.log(err));
+// Connect to MongoDB
+connectDB();
 
 // Routes
 app.get('/', (req, res) => res.send ('User Service Running'));
